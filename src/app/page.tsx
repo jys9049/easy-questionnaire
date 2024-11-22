c7f832858de60dcf86738203570db6b4884e874b
정용성 <jys9049@blocko.io>
2024-11-22 17:09:34 +0900

    feat: 배포 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ import { Container } from "@mui/material";
 import S from "./page.module.css";
 
 export default async function Home() {
-  return <Container className={S.container}>hi!!</Container>;
+  return <Container className={S.container}>hi!! deploy success!!</Container>;
 }
